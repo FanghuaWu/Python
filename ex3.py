e798502a50d2be29d15bfb646f8ef0edda690d7e
Fanghua Wu <fanghua.wu@anu.edu.au>
2017-10-19 16:49:45 +1100

    difference result between Python3 and Python2

--- ex3.py
+++ ex3.py
@@ -1,13 +1,14 @@
 print("I will now count my chickens:")
 print("Hens", 25 + 30 / 6)
 print("Roosters", 100 - 25 * 3 % 4)
-# *优先级最高，然后是%，最后是-
+#*优先级最高，然后是%，最后是-
 
 print("Now I will count the eggs:")
 
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# Python2结果为7， Python3结果为6.75
 print(3. + 2. + 1. - 5. + 4. % 2. - 1. / 4. + 6.)
-# 没有整型浮点型概念?
+# 6.75
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
