49cf5b1c6f459604e4d035278a248e05c08bff36
Fanghua Wu <fanghua.wu@anu.edu.au>
2017-10-19 16:40:07 +1100

    numbers and math

---
+++ ex3.py
@@ -0,0 +1,28 @@
+print("I will now count my chickens:")
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+# *优先级最高，然后是%，最后是-
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3. + 2. + 1. - 5. + 4. % 2. - 1. / 4. + 6.)
+# 没有整型浮点型概念?
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+# print 可以打印逻辑判断
+# print 字符型和逻辑型间用逗号隔开
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
