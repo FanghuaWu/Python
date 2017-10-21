467f32b7650559a52245fd4ddc360daa30e2620d
Fanghua Wu <fanghua.wu@anu.edu.au>
2017-10-21 17:10:49 +1100

    accessing elements of lists

---
+++ ex34.py
@@ -0,0 +1,7 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+order = ['1st', '2nd', '3rd', '4th', '5th']
+
+for i in range(0,5):
+    print(f"The animal at {i} is the {order[i]} and is a {animals[i]}.")
+
